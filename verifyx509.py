86b1b07473b5af6d6ddd54faa296bfcee9495806
Akash0299 <66462140+Akash0299@users.noreply.github.com>
2023-04-03 15:45:22 +0530

    Update verifyx509.py

--- verifyx509.py
+++ verifyx509.py
@@ -1,34 +1,42 @@
 import requests
 from cryptography import x509
 import paramiko
-from cryptography import x509
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives.hashes import SHA256
 from cryptography.hazmat.backends import default_backend
 
 def verifyX509(devicename,host, path):
+    #print(devicename,host, path)
     if host == 'localhost' or host == '127.0.0.1':
         resp1 = requests.get('http://127.0.0.1:8200/v1/pki/ca/pem')
+        #print(resp1)
         ca_cert_pem = resp1.text
+        #print(ca_cert_pem)
         DEVICE_CERT = path +"/"+devicename+"_cert.pem"
         DEVICE_KEY = path +"/"+devicename+"_key.pem"
+        #print(DEVICE_CERT,DEVICE_KEY)
         device_cert_pem = ''
         device_key_pem = ''
         with open(DEVICE_CERT, "r") as f:
-            f.read(device_cert_pem)
+            device_cert_pem = f.read()
         with open(DEVICE_KEY, "r") as f:
-            f.read(device_key_pem)
+            device_key_pem = f.read()
+            
+        #print(device_cert_pem)
+        #print(device_key_pem)
 
-        c = ca_cert_pem.split('\n')
-        cacert = bytes('\n'.join(c[1:len(c)-1]),'utf-8')
+        cacert = bytes(ca_cert_pem,'utf-8')
         ca_cert = x509.load_pem_x509_certificate(cacert, default_backend())
-
-        d = device_cert_pem.split('\n')
-        devcert = bytes('\n'.join(d[1:len(d)-1]),'utf-8')
+        print(ca_cert)
+        
+        devcert = bytes(device_cert_pem,'utf-8')
         device_cert = x509.load_pem_x509_certificate(devcert, default_backend())
+        print(device_cert)
 
         res = certverify(device_cert,ca_cert)
+        
+        print(res)
 
         return res
         
@@ -61,15 +69,17 @@ def verifyX509(devicename,host, path):
         with open(DEVICE_KEY, "r") as f:
             f.read(device_key_pem)
 
-        c = ca_cert_pem.split('\n')
-        cacert = bytes('\n'.join(c[1:len(c)-1]),'utf-8')
+        cacert = bytes(ca_cert_pem,'utf-8')
         ca_cert = x509.load_pem_x509_certificate(cacert, default_backend())
-
-        d = device_cert_pem.split('\n')
-        devcert = bytes('\n'.join(d[1:len(d)-1]),'utf-8')
+        print(ca_cert)
+        
+        devcert = bytes(device_cert_pem,'utf-8')
         device_cert = x509.load_pem_x509_certificate(devcert, default_backend())
+        print(device_cert)
 
         res = certverify(device_cert,ca_cert)
+        
+        print(res)
 
         return res
 
